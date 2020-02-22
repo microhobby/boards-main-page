77d30880cf8820d5d6e104792e99f8cec3e7385c
Matheus Castello <matheus@castello.eng.br>
2020-02-22 18:31:08 -0300

    Initial commit
    This is the initial style of main page
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+
+        <q-toolbar-title>
+          Boards Explorer Project
+        </q-toolbar-title>
+
+        <div>Boards v{{ boardsVersion }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label header class="text-grey-8">Boards List</q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink'
+
+export default {
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  data () {
+    return {
+      boardsVersion: '0.0',
+      leftDrawerOpen: false,
+      essentialLinks: [
+        {
+          title: 'Orange Pi Zero',
+          caption: 'Open-source single-board computer 48 mm × 46mm',
+          icon: 'developer_board',
+          link: 'https://quasar.dev'
+        },
+        {
+          title: 'Roduino',
+          caption: 'Homemade Arduino by Rodrigo Costa',
+          icon: 'developer_board',
+          link: 'https://github.com/quasarframework'
+        }
+      ]
+    }
+  },
+  created () {
+    console.log('Created')
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-page class="q-pa-md">
+
+    <div class="row">
+
+      <!-- Orange Pi -->
+      <div class="col">
+        <q-card class="my-card" flat bordered>
+          <q-card-section horizontal>
+            <q-img
+              class="col"
+              src="statics/1521880910218.boardFront.png"
+              style="background-color: #676767"
+            >
+              <div
+                :class="board1Trans"
+              >
+                <div class="text-h6">Orange Pi Zero</div>
+                <div class="text-subtitle2">Open-source single-board computer. 48 mm × 46mm</div>
+              </div>
+            </q-img>
+
+            <q-card-actions vertical class="justify-around q-px-md">
+              <q-btn flat round color="info" icon="info" @click="infoClick(1)" />
+              <q-btn flat round color="green" icon="play_arrow" />
+            </q-card-actions>
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <!-- Roduino -->
+      <div class="col">
+      </div>
+
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+
+  data () {
+    return {
+      board1Hide: false,
+      board1Trans: 'absolute-bottom hidden'
+    }
+  },
+  methods: {
+    infoClick (ix) {
+      console.log('Info Click ' + ix)
+      if (ix === 1) {
+        this.board1Hide = !this.board1Hide
+        this.board1Hide ? this.board1Trans = 'absolute-bottom animated slideInUp' : this.board1Trans = 'absolute-bottom animated slideOutDown'
+      }
+    }
+  }
+}
+</script>
